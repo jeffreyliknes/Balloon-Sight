39c4c3eeaf014042821cf79a0af925b4b1956779
Jeffrey Liknes <jeffreyliknes@Jeffreys-MacBook-Air-2.local>
2025-11-23 18:08:55 +0100

    Add Google Tag Manager (GTM-TM8CGZWC) to head and body

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,12 +79,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TM8CGZWC');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className={`${nunito.variable} ${fraunces.variable} font-sans min-h-screen bg-background text-foreground antialiased selection:bg-accent/20`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TM8CGZWC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div className="fixed inset-0 -z-10 h-full w-full bg-background opacity-100 pointer-events-none mix-blend-normal"></div>
         {children}
       </body>
